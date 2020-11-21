a8c811c670803c0c6b3299f31a051934990d1521
Samuel Štancl <samuel.stancl@gmail.com>
2020-11-21 16:58:52 +0100

    500 error page

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,6 @@
+<p>500 Error</p>
+
+<p>Seems like something is broken. In this demo, this is usually caused not by a bug, but by other users removing data in a way that breaks related resources.</p>
+<p>An unfortunate reality of letting anyone play with the database. I could've made it read-only, but then you couldn't try all of the features.</p>
+
+<p>The database gets refreshed every 3 minutes so please wait a bit and try refreshing the page.</p>
