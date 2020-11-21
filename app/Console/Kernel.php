398a3fda56d43e23012dd96d18b287622448c500
Samuel Štancl <samuel.stancl@gmail.com>
2020-11-21 13:49:21 +0100

    force scheduled command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('migrate:fresh', ['--seed'])->everyFiveMinutes();
+        $schedule->command('migrate:fresh', ['--seed' => true, '--force' => true])->everyFiveMinutes();
     }
 
     /**
